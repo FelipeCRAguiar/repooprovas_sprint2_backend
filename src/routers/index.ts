dc27b2658cab7467af47185140c6dfbb908695b0
Felipe <mr.tvz569@gmail.com>
2022-05-03 16:21:00 -0300

    small changes

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import categoryRouter from "./categoryRouter.js";
 import disciplineRouter from "./disciplineRouter.js";
-import postRouter from "./postRouter.js";
 import teacherRouter from "./teacherRouter.js";
 import testRouter from "./testRouter.js";
 import userRouter from "./userRouter.js";
@@ -12,5 +11,4 @@ router.use(testRouter);
 router.use(categoryRouter);
 router.use(disciplineRouter);
 router.use(teacherRouter);
-router.use(postRouter);
 export default router;
